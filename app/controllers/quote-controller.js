18d2355dd956e7421853682a03cd75a9f12a90a7
Zach Pfeifer <mr.zman1@yahoo.com>
2019-08-30 15:14:07 -0600

    Quote Prototype done*

--- app/controllers/quote-controller.js
+++ app/controllers/quote-controller.js
@@ -2,9 +2,22 @@ import QuoteService from "../services/quote-service.js";
 
 let _qs = new QuoteService()
 
+function _drawQuote() {
+  let quote = _qs.Quote
+  document.getElementById('quote').innerHTML = quote.Template
+
+  console.log("QUOTE ME B***", _qs.Quote);
+}
 
 //TODO Create methods for constructor, and rendering the quote to the page 
 //      (be sure to review the HTML as an element already was put there for you)
 export default class QuoteController {
+
+  constructor() {
+    //NOTE Register all subscribers
+    _qs.addSubscriber('quote', _drawQuote)
+    //NOTE Retrieve Data
+    _qs.getQuote()
+  }
 
 }
---
+++ app/models/quote.js
@@ -0,0 +1,25 @@
+export default class Quote {
+  constructor(data) {
+    console.log('[RAW QUOTE API DATA]', data);
+    this.body = data.quote.body
+    this.author = data.quote.author
+    this.tags = data.quote.tags
+  }
+
+  get Template() {
+    let template = `
+    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+        <div class="card-header">Quote of the Day</div>
+        <div class="card-body">
+          <h5 class="card-title">${this.author} Once Said:</h5>
+          <p class="card-text">
+          <i>"${this.body}"</i>
+          <br>
+          ${this.tags}
+          </p>
+        </div>
+    </div>
+    `
+    return template
+  }
+}
